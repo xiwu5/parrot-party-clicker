25ece4aaf440c4badbe5ec91c4de999a7e0372d3
Xi Wu <youngjoycewu@gmail.com>
2025-11-21 19:45:07 +0000

    add an effect when clicks to 5 and 10

--- src/index.js
+++ src/index.js
@@ -5,33 +5,39 @@ const state = {
 
 // 1. A click on the Party Parrot increases the counter by 1
 const registerEvents = () => {
-    imageSrc = "https://cultofthepartyparrot.com/parrots/hd/parrot.gif";
+    const imageSrc = "https://cultofthepartyparrot.com/parrots/hd/parrot.gif";
     const imgParrot = document.querySelector(`img[src="${imageSrc}"]`);
+    const parrotCountContainer = document.querySelector('#parrotCount');
+    const resetButton = document.querySelector('#resetButton');
+    
     imgParrot.addEventListener('click', () => {
         state.clicks += 1;
-        const parrotCountContainer = document.querySelector('#parrotCount');
         parrotCountContainer.textContent = `Clicks: ${state.clicks}`;
-    });
-};
+        // 3. Add an effect whenever the clickCount
+        //    reaches a specific number.
+        //    (Ex: make parrot bigger, smaller, change the text/emoji,
+        //    etc.)
 
-document.addEventListener('DOMContentLoaded', registerEvents);
+        if (state.clicks === 5) {
+            imgParrot.classList.add('big-parrot');
+        } else if (state.clicks === 10) {
+            imgParrot.classList.remove('big-parrot');
+            imgParrot.classList.add('small-parrot');
+        }
+    });
+
 
 // 2. Add a button called ‘reset’ that will reset the
 //    click count to ‘0’.
-const resetButton = document.querySelector('#resetButton');
-resetButton.addEventListener('click', () => {
+    resetButton.addEventListener('click', () => {
     state.clicks = 0;
-    const parrotCountContainer = document.querySelector('#parrotCount');
     parrotCountContainer.textContent = `Clicks: ${state.clicks}`;
-});
-
-// 3. Add an effect whenever the clickCount
-//    reaches a specific number.
-//    (Ex: make parrot bigger, smaller, change the text/emoji,
-//    etc.)
+    });
+};
 
 // Extra:
 // // Box 1: When the mouse hovers over Box 1, switch the background to pink
 // Box 2: When the spacebar is pressed down, make the text in Box 2 turn into a flan emoji
 // Box 3: When Box 3 is clicked on, make Party Parrot appear and disappear
 // Box 4: When Box 4 is double clicked, an alert shows up
+document.addEventListener('DOMContentLoaded', registerEvents);
